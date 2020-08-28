f6af899865d0b84af5d654132e9d1f7ff0ac54ac
Adithyan-KV <adithyankv007@gmail.com>
2020-08-29 02:09:21 +0530

    Retrieving data from API and displaying temperature
     -The temperature data from the JSON received via the api is now being
      extracted and displayed in the appropriate field on the page.

--- weather.js
+++ weather.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let api_key = config.API_KEY;
-    fetch(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`).then(response => response.json())
-        .then(data => console.log(data));
+    fetch(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`)
+        .then(response => response.json())
+        .then(data => {
+            tempKelvin = data.main.temp;
+            tempCelsius = Math.round(tempKelvin - 273)
+            setTemperature(tempCelsius);
+        });
+
+    setTemperature = (temp) => {
+        let tempDisplay = document.querySelector('.magnitude');
+        tempDisplay.innerHTML = `${temp}&deg;`;
+    }
 });
