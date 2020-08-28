a078421ae60882e8752da170527585dae97111c2
Adithyan-KV <adithyankv007@gmail.com>
2020-08-28 23:32:54 +0530

    config file was added to manage API keys
     -The API key was stored in a seperate config file
     -The file has been added to gitignore

--- weather.js
+++ weather.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London&appid=apikeygoeshere').then(response => response.json())
+
+    let api_key = config.API_KEY;
+    fetch(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`).then(response => response.json())
         .then(data => console.log(data));
 });
