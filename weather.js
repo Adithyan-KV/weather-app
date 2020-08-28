a91f3f84c396d6e59fde091a3d44823d9aee728a
Adithyan-KV <adithyankv007@gmail.com>
2020-08-29 02:34:20 +0530

    Added city name and weather description

--- weather.js
+++ weather.js
@@ -4,13 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             tempKelvin = data.main.temp;
             tempCelsius = Math.round(tempKelvin - 273)
+            city = data.name;
+            weather = data.weather[0].description;
+            setCity(city);
             setTemperature(tempCelsius);
+            setWeather(weather);
         });
 
     setTemperature = (temp) => {
         let tempDisplay = document.querySelector('.magnitude');
         tempDisplay.innerHTML = `${temp}&deg;`;
+    }
+
+    setCity = (city) => {
+        let cityName = document.querySelector('.city');
+        cityName.innerHTML = city;
+    }
+
+    setWeather = (weather) => {
+        let description = document.querySelector('.description');
+        description.innerHTML = weather;
     }
 });
