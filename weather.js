c1adb11c4618226e268f8ba9e9044424fa887908
Adithyan-KV <adithyankv007@gmail.com>
2020-08-29 23:47:45 +0530

    City input added
     -Now a custom city name can be inputed and the weather for the
      particular city will be shown

--- weather.js
+++ weather.js
@@ -1,18 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let api_key = config.API_KEY;
-    fetch(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            tempKelvin = data.main.temp;
-            tempCelsius = Math.round(tempKelvin - 273)
-            city = data.name;
-            weather = data.weather[0].description;
-            setCity(city);
-            setTemperature(tempCelsius);
-            setWeather(weather);
-        });
+    let cityForm = document.querySelector('#city-input');
+    cityForm.addEventListener('submit', (event) => {
+
+        event.preventDefault();
+        cityInput = document.querySelector('#city-name');
+        getWeatherFor(cityInput.value);
+        cityForm.reset();
+
+        return false;
+    })
+
+    getWeatherFor = (city) => {
+        fetch(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                tempKelvin = data.main.temp;
+                tempCelsius = Math.round(tempKelvin - 273)
+                city = data.name;
+                weather = data.weather[0].description;
+                setCity(city);
+                setTemperature(tempCelsius);
+                setWeather(weather);
+                data = document.querySelector('.data');
+                if (!data.classList.contains('active')) {
+                    data.classList.add('active');
+                }
+            });
+    }
+
 
     setTemperature = (temp) => {
         let tempDisplay = document.querySelector('.magnitude');
