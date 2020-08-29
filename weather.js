333d791caeb1e43e829489414912a588545a52cb
Adithyan-KV <adithyankv007@gmail.com>
2020-08-30 02:32:00 +0530

    Implemented Temperature units toggle
     -The temperature can now be seen in either celsius or farenheit units
     -Associated styling was also changed.

--- weather.js
+++ weather.js
@@ -2,6 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let api_key = config.API_KEY;
     let cityForm = document.querySelector('#city-input');
+    let unitChanger = document.querySelectorAll('.units');
+    let unit = 'c';
+    let tempKelvin = 0;
+    let temp = 0;
+
+    unitChanger.forEach(item => {
+        item.addEventListener('click', () => {
+            if (item.id === 'celsius') {
+                unitChanger[0].classList.remove('passive');
+                unitChanger[1].classList.add('passive');
+                unit = 'c';
+                temp = getConvertedTemp(tempKelvin, 'c');
+            }
+            else {
+                unitChanger[1].classList.remove('passive');
+                unitChanger[0].classList.add('passive');
+                unit = 'f';
+                temp = getConvertedTemp(tempKelvin, 'f');
+            }
+            setTemperature(temp);
+        });
+    })
+
     cityForm.addEventListener('submit', (event) => {
 
         event.preventDefault();
@@ -12,25 +35,36 @@ document.addEventListener('DOMContentLoaded', () => {
         return false;
     })
 
+
     getWeatherFor = (city) => {
         fetch(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
                 tempKelvin = data.main.temp;
-                tempCelsius = Math.round(tempKelvin - 273)
-                city = data.name;
-                weather = data.weather[0].description;
+                temp = getConvertedTemp(tempKelvin, unit);
+                let city = data.name;
+                let weather = data.weather[0].description;
                 setCity(city);
-                setTemperature(tempCelsius);
+                setTemperature(temp);
                 setWeather(weather);
                 data = document.querySelector('.data');
                 if (!data.classList.contains('active')) {
                     data.classList.add('active');
                 }
             });
-    }
+    };
 
+    getConvertedTemp = (tempKelvin, unit) => {
+        if (unit === 'c') {
+            temp = Math.round(tempKelvin - 273)
+            return (temp);
+        }
+        else {
+            temp = Math.round((tempKelvin - 273) * (9 / 5) + 32);
+            return (temp);
+        }
+    };
 
     setTemperature = (temp) => {
         let tempDisplay = document.querySelector('.magnitude');
